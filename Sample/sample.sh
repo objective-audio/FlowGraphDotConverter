eb99328e00cd84ad052f1b5758b6a5c53c84a812
Yuki Yasoshima <yukiyasos@gmail.com>
2018-06-07 10:38:53 +0900

    サンプルの修正

--- Sample/sample.sh
+++ Sample/sample.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 cd `dirname $0`
+
+if ! which brew; then
+  echo "brew not found. Prease install Homebrew."
+  exit 1
+fi
+
+if ! brew list | grep -q graphviz; then
+  brew install graphviz
+fi
+
 swift build -c release
 ../.build/release/FlowGraphDotConverter ../Sources/FlowGraphDotConverterCore/FlowGraphScanner.swift --output ./ --noenter
 dot -T svg ./StateScanner.dot -o ./StateScanner.svg
