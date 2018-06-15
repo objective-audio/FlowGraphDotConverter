bf37eeac4caee703c824993456e7232f65e79965
Yuki Yasoshima <yukiyasos@gmail.com>
2018-06-15 19:07:56 +0900

    SwiftFlowGraphのバージョンを指定

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/jpsim/SourceKitten.git", from: "0.21.0"),
         .package(url: "https://github.com/kylef/Commander.git", from: "0.8.0"),
-        .package(url: "https://github.com/objective-audio/SwiftFlowGraph.git", .branch("separate_wait_run")),
+        .package(url: "https://github.com/objective-audio/SwiftFlowGraph.git", from: "0.2.0"),
     ],
     targets: [
         .target(
