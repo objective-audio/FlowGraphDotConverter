c5152bff2c210ef06e140382001232bd8fbb01f3
Yuki Yasoshima <yukiyasos@gmail.com>
2018-07-04 16:34:04 +0900

    オプションのnoenterをhide-enterに変更

--- Sources/FlowGraphDotConverter/main.swift
+++ Sources/FlowGraphDotConverter/main.swift
@@ -6,7 +6,7 @@ import Foundation
 import Commander
 import FlowGraphDotConverterCore
 
-let main = command (Option("output", default: "", description: "Output Directory Path"), Flag("noenter", description: "Exclude Enter States"), VariadicArgument<String>("inFilePaths", description: "Input File Paths")) { (outDirPath, enter, inFilePaths) in
+let main = command (Option("output", default: "", description: "Output Directory Path"), Flag("hide-enter", description: "Hide Enter States"), VariadicArgument<String>("inFilePaths", description: "Input File Paths")) { (outDirPath, enter, inFilePaths) in
     for inFilePath in inFilePaths {
         FlowGraphDotConverter.convert(inFilePath: inFilePath, outDirPath: outDirPath, isRemoveEnter: enter)
     }
