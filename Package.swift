11b990960978ffca5ef6afd4cd3da4b4c14377a8
Yuki Yasoshima <yukiyasos@gmail.com>
2018-07-23 22:53:01 +0900

    Update package version

--- Package.swift
+++ Package.swift
@@ -10,9 +10,9 @@ let package = Package(
         .library(name: "FlowGraphDotConverterCore", targets: ["FlowGraphDotConverterCore"]),
         ],
     dependencies: [
-        .package(url: "https://github.com/jpsim/SourceKitten.git", from: "0.21.0"),
+        .package(url: "https://github.com/jpsim/SourceKitten.git", from: "0.21.1"),
         .package(url: "https://github.com/kylef/Commander.git", from: "0.8.0"),
-        .package(url: "https://github.com/objective-audio/SwiftFlowGraph.git", from: "0.2.0"),
+        .package(url: "https://github.com/objective-audio/SwiftFlowGraph.git", from: "0.2.1"),
     ],
     targets: [
         .target(
