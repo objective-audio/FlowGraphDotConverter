76891307253d6d07642776f494962761f2caa661
Yuki Yasoshima <yukiyasos@gmail.com>
2018-08-08 17:48:24 +0900

    main.swiftの整理

--- Sources/FlowGraphDotConverter/main.swift
+++ Sources/FlowGraphDotConverter/main.swift
@@ -6,7 +6,9 @@ import Foundation
 import Commander
 import FlowGraphDotConverterCore
 
-let main = command (Option("output", default: "", description: "Output Directory Path"), Flag("hide-enter", description: "Hide Enter States"), VariadicArgument<String>("inFilePaths", description: "Input File Paths")) { (outDirPath, enter, inFilePaths) in
+let main = command (Option("output", default: "", description: "Output Directory Path"),
+                    Flag("hide-enter", description: "Hide Enter States"),
+                    VariadicArgument<String>("inFilePaths", description: "Input File Paths")) { (outDirPath, enter, inFilePaths) in
     for inFilePath in inFilePaths {
         FlowGraphDotConverter.convert(inFilePath: inFilePath, outDirPath: outDirPath, isRemoveEnter: enter)
     }
