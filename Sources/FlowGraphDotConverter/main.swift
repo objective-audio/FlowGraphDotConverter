197edbc5c3654ddb17dc672b28ed241aa78e1a9a
Yuki Yasoshima <yasoshima@ubiregi.com>
2019-12-03 19:11:46 +0900

    fix VariadicArgument

--- Sources/FlowGraphDotConverter/main.swift
+++ Sources/FlowGraphDotConverter/main.swift
@@ -8,7 +8,7 @@ import FlowGraphDotConverterCore
 
 let main = command (Option("output", default: "", description: "Output Directory Path"),
                     Flag("hide-enter", description: "Hide Enter States"),
-                    VariadicArgument<String>("inFilePaths", description: "Input File Paths")) { (outDirPath, enter, inFilePaths) in
+                    Argument<Array<String>>("inFilePaths", description: "Input File Paths")) { (outDirPath, enter, inFilePaths) in
     for inFilePath in inFilePaths {
         FlowGraphDotConverter.convert(inFilePath: inFilePath, outDirPath: outDirPath, isRemoveEnter: enter)
     }
